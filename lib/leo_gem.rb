612850f599e076751a5aebfc1563944ac31d20d8
netznarkose <mmaintz@posteo.de>
2017-03-19 17:16:33 +0100

    refactor get_url

--- lib/leo_gem.rb
+++ lib/leo_gem.rb
@@ -1,6 +1,26 @@
-def compose_url(language = 'ende', search_term = 'hello')
+require 'pry-byebug'
+require 'nokogiri'
+require 'open-uri'
+
+def compose_query(language = 'ende', search_term = 'hello')
   url = 'https://dict.leo.org/dictQuery/m-vocab/' << language
   url << '/query.xml?tolerMode=nof&lp=ende&lang=en&rmWords=off&rmSearch=on&directN=0&search='
   url << search_term
   url << '&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16'
+end
+
+def instantiate_nokogiri_object(url)
+  Nokogiri::XML(open(url))
+rescue
+  :danger
+end
+
+def parse_audio_identifier(url)
+  instantiate_nokogiri_object(url).at_css('pron').first[1]
+rescue
+  :danger
+end
+
+def compose_url(url)
+  "https://dict.leo.org/media/audio/#{parse_audio_identifier(url)}.mp3"
 end
