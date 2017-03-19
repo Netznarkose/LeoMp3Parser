d469b62cbdcdd241c316fc81184eecfc34743fd4
netznarkose <mmaintz@posteo.de>
2017-03-19 15:24:04 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+group :test do
+  gem 'rspec'
+end
+
+group :test, :develpment do
+  gem 'nokogiri'
+  gem 'pry-byebug'
+end
---
+++ lib/leo_gem.rb
@@ -0,0 +1,9 @@
+# Leo Gem
+
+def compose_url(language = 'ende', search_term = 'hello')
+	url = "https://dict.leo.org/dictQuery/m-vocab/" << language
+	url << "/query.xml?tolerMode=nof&lp=ende&lang=en&rmWords=off&rmSearch=on&directN=0&search="
+	url << search_term
+	url << "&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"
+	# get_url(url)
+end
