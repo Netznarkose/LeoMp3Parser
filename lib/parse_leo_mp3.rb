7c0852a5c87156f897a05b06fe264c6cd6cdda94
netznarkose <mmaintz@posteo.de>
2017-03-21 10:47:42 +0100

    add argument validation

--- lib/parse_leo_mp3.rb
+++ lib/parse_leo_mp3.rb
@@ -2,15 +2,26 @@ require 'pry-byebug'
 require 'nokogiri'
 require 'open-uri'
 class ParseLeoMp3
-  # attr_accessor :language, :term
-  # attr_reader :url
-  # def initialize(input_hash = {})
-  #   @language_and_term = input_hash || { language: 'ende', term: 'hello' }
-  #   @url = compose_query(@language, @term)
+  # attr_accessor :language_and_term
+  # def initialize(language_and_term = {})
+  #   validate_arguments(language_and_term)
+  #   @language_and_term = language_and_term
   # end
 
-  # write an input-validation
+  def validate_arguments(language_and_term)
+    raise ArgumentError.new('argument must be a Hash') unless language_and_term.is_a? Hash
+    raise ArgumentError.new('Keys must be :language and :term') unless (language_and_term.keys.include?(:language) && language_and_term.keys.include?(:term))
+    language_and_term.values.map do |value|
+      raise ArgumentError.new('Values must be Strings') unless value.is_a? String
+    end
+    language_and_term.values.map do |value|
+      raise ArgumentError.new('Values can not be emtpy') if value.empty?
+    end
+    raise ArgumentError.new('Language must be set to ende, esde or frde') unless (language_and_term[:language].include?('ende') || language_and_term[:language].include?('esde') || language_and_term[:language].include?('frde'))
+  end
+
   def get_audio_url(language_and_term)
+    validate_arguments(language_and_term)
     url = compose_query(language_and_term)
     compose_url(url)
   end
--- spec/parse_leo_mp3_spec.rb
+++ spec/parse_leo_mp3_spec.rb
@@ -4,10 +4,31 @@ RSpec.describe "parse_leo_mp3" do
   let(:parse_leo_mp3) { ParseLeoMp3.new }
   let(:correct_url) { 'https://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=en&rmWords=off&rmSearch=on&directN=0&search=hello&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16' }
   let(:false_url) { 'false_url' }
+  let(:language_and_term) { { language: 'ende', term: 'hello' } }
 
+  describe '.validate_arguments' do
+    it 'does not raise an error whith correct arguments' do
+      expect{ parse_leo_mp3.validate_arguments(language_and_term) }.not_to raise_error
+    end
+    it 'raises an error when argument is not a hash' do
+      expect { parse_leo_mp3.validate_arguments('not a hash') }.to raise_error(ArgumentError, 'argument must be a Hash')
+    end
+    it 'raises an error when keys are not :language and :term' do
+      expect { parse_leo_mp3.validate_arguments({ number: 'ende', color: 'hello' }) }.to raise_error(ArgumentError, 'Keys must be :language and :term')
+    end
+    it 'raises an error when values are not Strings' do
+      expect { parse_leo_mp3.validate_arguments({ language: 23, term: :blue }) }.to raise_error(ArgumentError, 'Values must be Strings')
+    end
+    it 'raises an error when values are empty' do
+      expect { parse_leo_mp3.validate_arguments({ language: '', term: '' }) }.to raise_error(ArgumentError, 'Values can not be emtpy')
+    end
+    it 'raises an error unless language is ende, esde or frde' do
+      expect { parse_leo_mp3.validate_arguments({ language: 'wrong_language_selection', term: 'hello' }) }.to raise_error(ArgumentError, 'Language must be set to ende, esde or frde')
+    end
+  end
   describe '.compose_query' do
     it 'retuns correct default url' do
-      expect(parse_leo_mp3.compose_query).to eq(correct_url)
+      expect(parse_leo_mp3.compose_query(language_and_term)).to eq(correct_url)
     end
 
     describe '.instantiate_nokogiri_object' do
